a8ec1de0fe484153110aec5dc779fe76b051edb6
Rhys Lindhorst <lindhorst@chapman.edu>
2025-11-09 10:30:02 -0800

    changes

--- maxscore.sh
+++ maxscore.sh
@@ -4,19 +4,19 @@
 
 echo "Enter 5 scores:"
 for i in {0..4}; do
-    read -r score[$i]
+    read -r score["$i"]
 done
 
 max=${score[0]}
 for i in {1..4}; do
-    if [ "${score[$i]}" -gt "$max" ]; then
-        max=${score[$i]}
+    if (( score[i] > max )); then
+        max=${score[i]}
     fi
 done
 
 echo "The highest score is $max"
 echo "The scores are:"
 for i in {0..4}; do
-    diff=$((max - score[$i]))
-    echo "${score[$i]} differs from max by $diff"
+    diff=$((max - score[i]))
+    echo "${score[i]} differs from max by $diff"
 done
